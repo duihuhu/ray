cf133bd7dc9f25489a15fa232cf68e5fd3d91675
duihuhu <duihuhu@163.com>
2022-09-24 15:25:05 +0800

    add log reqeust

--- src/ray/rpc/client_call.h
+++ src/ray/rpc/client_call.h
@@ -235,6 +235,8 @@ class ClientCallManager {
       const ClientCallback<Reply> &callback,
       std::string call_name,
       int64_t method_timeout_ms = -1) {
+    
+    RAY_LOG(INFO) << request << "\n\n";
     auto stats_handle = main_service_.stats().RecordStart(call_name);
     if (method_timeout_ms == -1) {
       method_timeout_ms = call_timeout_ms_;
