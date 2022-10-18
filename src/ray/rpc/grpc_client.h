a6e3805ab389a153a121624fc8f5f7f49daa0a85
duihuhu <duihuhu@163.com>
2022-10-18 10:00:34 +0800

    delete RAY_LOG

--- src/ray/rpc/grpc_client.h
+++ src/ray/rpc/grpc_client.h
@@ -29,7 +29,7 @@ namespace rpc {
 
 // This macro wraps the logic to call a specific RPC method of a service,
 // to make it easier to implement a new RPC client.
-// RAY_LOG(INFO) << "HUCC\n\n" << rpc_client.channel_.address <<"\n\n"; \
+// RAY_LOG(INFO) << "HUCC\n\n" << rpc_client.channel_.address <<"\n\n"; 
 
 #define INVOKE_RPC_CALL(                                               \
     SERVICE, METHOD, request, callback, rpc_client, method_timeout_ms) \
