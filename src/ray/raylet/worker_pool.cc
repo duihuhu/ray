c705dfb3b3bd06f4c5772aa0ed53027ecca7e651
duihuhu <duihuhu@163.com>
2022-10-24 18:07:01 +0800

    debug grpc

--- src/ray/raylet/worker_pool.cc
+++ src/ray/raylet/worker_pool.cc
@@ -1000,20 +1000,6 @@ void WorkerPool::PushWorker(const std::shared_ptr<WorkerInterface> &worker) {
 }
 
 void WorkerPool::TryKillingIdleWorkers() {
-  int i = 0;
-  if(GetAllRegisteredDrivers().size()<=0) {
-    RAY_LOG(INFO) << "hucc no driver : " << i << "\n";
-  }
-  for (const auto &drive : GetAllRegisteredDrivers()) {
-    if (drive->IsDead()) {
-      i = i + 1;
-      RAY_LOG(INFO) << "hucc driver dead: " << i << "\n";
-    } else {
-      RAY_LOG(INFO) << "hucc driver not dead: " << i << "\n";
-    }
-  }
-  
-
   RAY_CHECK(idle_of_all_languages_.size() == idle_of_all_languages_map_.size());
 
   int64_t now = get_time_();
@@ -1147,13 +1133,26 @@ void WorkerPool::TryKillingIdleWorkers() {
                 // kill the worker (e.g., when the worker owns the object). Without this,
                 // if the first N workers own objects, it can't kill idle workers that are
                 // >= N+1.
-                const auto job_id = worker->GetAssignedJobId();
-                RAY_LOG(INFO) << "hucc try again: " << job_id.IsNil() << "job id: " << job_id << "\n";
-                const auto &idle_pair = idle_of_all_languages_.front();
-                idle_of_all_languages_.push_back(idle_pair);
-                idle_of_all_languages_.pop_front();
-                RAY_CHECK(idle_of_all_languages_.size() ==
-                          idle_of_all_languages_map_.size());
+                int i = 0;
+                if(GetAllRegisteredDrivers().size()<=0) {
+                  RAY_LOG(INFO) << "hucc no driver : " << i << "\n";
+                
+                // for (const auto &drive : GetAllRegisteredDrivers()) {
+                //   if (drive->IsDead()) {
+                //     i = i + 1;
+                //     RAY_LOG(INFO) << "hucc driver dead: " << i << "\n";
+                //   } else {
+                //     RAY_LOG(INFO) << "hucc driver not dead: " << i << "\n";
+                //   }
+                // }
+                  const auto job_id = worker->GetAssignedJobId();
+                  RAY_LOG(INFO) << "hucc try again: " << job_id.IsNil() << "job id: " << job_id << "\n";
+                  const auto &idle_pair = idle_of_all_languages_.front();
+                  idle_of_all_languages_.push_back(idle_pair);
+                  idle_of_all_languages_.pop_front();
+                  RAY_CHECK(idle_of_all_languages_.size() ==
+                            idle_of_all_languages_map_.size());
+                }
               }
             });
       } else {
