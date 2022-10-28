2f30c4a6dad218791b87735e53a0c99de7916815
duihuhu <duihuhu@163.com>
2022-10-28 11:53:07 +0800

    add time for submittask

--- src/ray/core_worker/store_provider/plasma_store_provider.cc
+++ src/ray/core_worker/store_provider/plasma_store_provider.cc
@@ -276,8 +276,8 @@ Status CoreWorkerPlasmaStoreProvider::Get(
   // First, attempt to fetch all of the required objects once without reconstructing.
   std::vector<ObjectID> id_vector(object_ids.begin(), object_ids.end());
   int64_t total_size = static_cast<int64_t>(object_ids.size());
-  // hucc time for get obj from local plasma
-  // auto ts_get_obj_local_plasma = current_sys_time_us();
+  hucc time for get obj from local plasma
+  auto ts_get_obj_local_plasma = current_sys_time_us();
   
   for (int64_t start = 0; start < total_size; start += batch_size) {
     batch_ids.clear();
@@ -293,8 +293,8 @@ Status CoreWorkerPlasmaStoreProvider::Get(
                                                  results,
                                                  got_exception));
   }
-  // auto te_get_obj_local_plasma = current_sys_time_us();
-  // RAY_LOG(INFO) << "hucc time for get obj from local plasma total time: " << te_get_obj_local_plasma - ts_get_obj_local_plasma << "\n";
+  auto te_get_obj_local_plasma = current_sys_time_us();
+  RAY_LOG(INFO) << "hucc time for get obj from local plasma total time: " << te_get_obj_local_plasma << "," << ts_get_obj_local_plasma << "empty: " << remaining.empty() << "\n";
   // If all objects were fetched already, return. Note that we always need to
   // call UnblockIfNeeded() to cancel the get request.
   if (remaining.empty() || *got_exception) {
@@ -331,6 +331,7 @@ Status CoreWorkerPlasmaStoreProvider::Get(
       RAY_RETURN_NOT_OK(raylet_client_->NotifyDirectCallTaskBlocked(
           /*release_resources_during_plasma_fetch=*/false));
     }
+
     RAY_RETURN_NOT_OK(FetchAndGetFromPlasmaStore(remaining,
                                                  batch_ids,
                                                  batch_timeout,
