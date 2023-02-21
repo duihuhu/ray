84e22e3446bf28335206a4c0b2bcf05d08d0ed9d
duihuhu <duihuhu@163.com>
2023-02-21 11:32:59 +0800

    debug add doca header

--- src/ray/object_manager/plasma/secure_channel/secure_channel_core.cc
+++ src/ray/object_manager/plasma/secure_channel/secure_channel_core.cc
@@ -677,7 +677,9 @@ start_threads(struct cc_ctx *ctx)
 doca_error_t
 sc_start(struct sc_config *cfg, struct cc_ctx *ctx)
 {
-	struct t_results t_results = {0};
+	struct t_results t_results;
+	// = {0};
+	memset(t_results, 0, sizeof(t_results));
 	struct doca_dev *dev = NULL;
 	struct doca_dev_rep *dev_rep = NULL;
 	doca_error_t result;
