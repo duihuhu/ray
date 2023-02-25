28e18dd6bdc9f6f335e2b1e003071e19cf63037a
duihuhu <duihuhu@163.com>
2023-02-25 18:40:40 +0800

    debug InitConnChannel function

--- src/ray/object_manager/plasma/store.cc
+++ src/ray/object_manager/plasma/store.cc
@@ -162,8 +162,8 @@ void PlasmaStore::RunCommService(int index) {
       if (!allocation.has_value()) {
         std::cout<< "allocation no value" <<"\n";
       }
-      std::cout << "allocation address: " << allocation.address  << "allocation size: " << allocation.size  \
-        << "allocation device_num: " << allocation.device_num  << "allocation mmap_size: " << allocation.mmap_size;
+      std::cout << "allocation address: " << allocation->address  << "allocation size: " << allocation->size  \
+        << "allocation device_num: " << allocation->device_num  << "allocation mmap_size: " << allocation->mmap_size;
 
       auto ptr = std::make_unique<LocalObject>(std::move(allocation.value()));
       auto entry =
