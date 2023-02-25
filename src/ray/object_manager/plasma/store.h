b16b7c73a2f9539f384f8239025cf7b06cad2d3d
duihuhu <duihuhu@163.com>
2023-02-25 15:19:04 +0800

    debug InitConnChannel function

--- src/ray/object_manager/plasma/store.h
+++ src/ray/object_manager/plasma/store.h
@@ -43,6 +43,7 @@
 #include "ray/object_manager/plasma/protocol.h"
 // #include "secure_channel/secure_channel.h"
 #include "secure_channel/include/doca_comm_channel.h"
+#include "ray/common/asio/periodical_runner.h"
 // #define META_NAME_LENGTH 20;
 
 namespace plasma {
