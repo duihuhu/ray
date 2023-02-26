33bdd6effeea9b6502ac92cc9f18f90143943efa
duihuhu <duihuhu@163.com>
2023-02-26 11:43:52 +0800

    debug InitConnChannel function

--- src/ray/object_manager/plasma/object_store.cc
+++ src/ray/object_manager/plasma/object_store.cc
@@ -25,6 +25,8 @@ ObjectStore::ObjectStore(IAllocator &allocator)
 const LocalObject *ObjectStore::CreateObject(const ray::ObjectInfo &object_info,
                                              plasma::flatbuf::ObjectSource source,
                                              bool fallback_allocate) {
+  std::cout<< "attempting to create object " << object_info.object_id << " size "
+                 << object_info.data_size <<sdt::endl;
   RAY_LOG(DEBUG) << "attempting to create object " << object_info.object_id << " size "
                  << object_info.data_size;
   RAY_CHECK(object_table_.count(object_info.object_id) == 0)
