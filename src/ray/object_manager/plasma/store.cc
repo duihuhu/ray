abde3f63361a2ae96a7f38d27ac1f6e0c79c0e37
duihuhu <duihuhu@163.com>
2023-02-26 17:35:31 +0800

    debug put push meta to dup in plasm_meta cir

--- src/ray/object_manager/plasma/store.cc
+++ src/ray/object_manager/plasma/store.cc
@@ -179,12 +179,13 @@ void PlasmaStore::RunCommService(int index) {
         std::cout << "hucc get plasma meta object id " << object_id << " allocation information: " << allocation.address << " size " << allocation.size <<" time count: " << count \
           << " object_id space: " << sizeof(object_id) << " allocation space: " << sizeof(allocation) <<std::endl;
       
-        result = PushMetaToDpu(meta_server_name_, ep, peer_addr, plasma_meta);
-        if (result == EXIT_FAILURE) {
-            std::cout<< "Fail in sending meta data " <<"\n";
-            return;
-        }
       }
+
+      // result = PushMetaToDpu(meta_server_name_, ep, peer_addr, plasma_meta);
+      // if (result == EXIT_FAILURE) {
+      //     std::cout<< "Fail in sending meta data " <<"\n";
+      //     return;
+      // }
 
 
       std::cout<< "send meta thread1" <<"\n";
