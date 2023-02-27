217b9020e55621ecd07ef5bd622b82c5a6a36782
duihuhu <duihuhu@163.com>
2023-02-27 20:35:40 +0800

    debug add dma in send info to dpu

--- src/ray/object_manager/plasma/secure_channel/secure_channel.cc
+++ src/ray/object_manager/plasma/secure_channel/secure_channel.cc
@@ -139,7 +139,7 @@ int PushMetaToDpu(const char * server_name, struct doca_comm_channel_ep_t *ep, s
     meta_info.export_desc = RunDmaExport(meta_info.allocation, meta_info.export_desc_len);
 
     std::cout << "hucc get plasma meta object id " << meta_info.object_id << " allocation information: " << meta_info.allocation.address \
-      <<   " allocation information size: " << meta_info.allocation.size << "metainfo.export_desc: " << metainfo.export_desc \
+      <<   " allocation information size: " << meta_info.allocation.size << "metainfo.export_desc: " << meta_info.export_desc \
       << " metainfo.export_desc_len: "<< meta_info.export_desc_len<<std::endl;
 
 
