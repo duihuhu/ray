91abfdc85846fa0a37ef6ace50e9761250b7f831
duihuhu <duihuhu@163.com>
2023-02-28 16:17:57 +0800

    debug object deserialization

--- src/ray/object_manager/plasma/object_store.cc
+++ src/ray/object_manager/plasma/object_store.cc
@@ -25,10 +25,10 @@ ObjectStore::ObjectStore(IAllocator &allocator)
 const LocalObject *ObjectStore::CreateObject(const ray::ObjectInfo &object_info,
                                              plasma::flatbuf::ObjectSource source,
                                              bool fallback_allocate) {
-  std::cout<< "attempting to create object " << object_info.object_id << " size "
-                 << object_info.data_size <<std::endl;
+  // std::cout<< "attempting to create object " << object_info.object_id << " size "
+  //                << object_info.data_size <<std::endl;
   RAY_LOG(DEBUG) << "attempting to create object " << object_info.object_id << " size "
-                 << object_info.data_size;
+                 << object_info.data_size <<  " meta size " << object_info.data_size;
   RAY_CHECK(object_table_.count(object_info.object_id) == 0)
       << object_info.object_id << " already exists!";
   auto object_size = object_info.GetObjectSize();
