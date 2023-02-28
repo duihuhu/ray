abb0a31474c8bcf2e37e1a3582a52b4ca85c3d34
duihuhu <duihuhu@163.com>
2023-02-28 16:36:23 +0800

    debug object deserialization

--- src/ray/object_manager/plasma/object_store.cc
+++ src/ray/object_manager/plasma/object_store.cc
@@ -25,8 +25,7 @@ ObjectStore::ObjectStore(IAllocator &allocator)
 const LocalObject *ObjectStore::CreateObject(const ray::ObjectInfo &object_info,
                                              plasma::flatbuf::ObjectSource source,
                                              bool fallback_allocate) {
-  std::cout<< "attempting to create object " << object_info.object_id << " size "
-                 << object_info.data_size <<  " meta size " << object_info.metadata_size <<std::endl;
+
   RAY_LOG(DEBUG) << "attempting to create object " << object_info.object_id << " size "
                  << object_info.data_size <<  " meta size " << object_info.metadata_size;
   RAY_CHECK(object_table_.count(object_info.object_id) == 0)
@@ -40,6 +39,7 @@ const LocalObject *ObjectStore::CreateObject(const ray::ObjectInfo &object_info,
   if (!allocation.has_value()) {
     return nullptr;
   }
+  std::cout<< "attempting to create object size" << object_size <<std::endl;
   auto ptr = std::make_unique<LocalObject>(std::move(allocation.value()));
   auto entry =
       object_table_.emplace(object_info.object_id, std::move(ptr)).first->second.get();
