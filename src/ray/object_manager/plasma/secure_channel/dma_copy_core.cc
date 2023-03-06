9ed70ddc01abd45ad72a911b0cfefc68bb26cd6c
duihuhu <duihuhu@163.com>
2023-03-06 09:47:32 +0800

    debug covert metainfo to basemetainfo

--- src/ray/object_manager/plasma/secure_channel/dma_copy_core.cc
+++ src/ray/object_manager/plasma/secure_channel/dma_copy_core.cc
@@ -1219,7 +1219,7 @@ host_start_dma_copy(struct dma_copy_cfg *dma_cfg, struct core_state *core_state,
 	// if (result != DOCA_SUCCESS)
 	// 	return result;
 
-  result = doca_mmap_populate(core_state->mmap, (char*)metainfo.address+metainfo.offset, metainfo.size, sysconf(_SC_PAGESIZE), NULL, NULL);
+  result = doca_mmap_populate(core_state->mmap, ((char*)(metainfo.address))+metainfo.offset, metainfo.size, sysconf(_SC_PAGESIZE), NULL, NULL);
 
 	if (result != DOCA_SUCCESS) {
 		DOCA_LOG_ERR("Unable to populate memory map: %s", doca_get_error_string(result));
