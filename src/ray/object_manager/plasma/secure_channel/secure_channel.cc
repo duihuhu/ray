fb0350709415bc988a7943443b7a94eb7a9f9001
duihuhu <duihuhu@163.com>
2023-03-06 09:44:29 +0800

    debug covert metainfo to basemetainfo

--- src/ray/object_manager/plasma/secure_channel/secure_channel.cc
+++ src/ray/object_manager/plasma/secure_channel/secure_channel.cc
@@ -95,7 +95,8 @@ struct BaseMetaInfo {
   ptrdiff_t offset;
   size_t export_desc_len;
   char export_desc[CC_EXPORT_DESC_SIZE] = {0};
-  MetaInfo(unsigned long address, int64_t size, size_t export_desc_len) {
+  BaseMetaInfo(){}
+  BaseMetaInfo(unsigned long address, int64_t size, size_t export_desc_len) {
     this->address = address;
     this->size = size;
     this->export_desc_len =export_desc_len;
