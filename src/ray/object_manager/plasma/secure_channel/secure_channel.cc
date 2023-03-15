13367b6594844c30fb8eb85846db0e0fb415282a
duihuhu <duihuhu@163.com>
2023-03-15 21:01:27 +0800

    debug delete daylet process

--- src/ray/object_manager/plasma/secure_channel/secure_channel.cc
+++ src/ray/object_manager/plasma/secure_channel/secure_channel.cc
@@ -132,7 +132,7 @@ doca_error_t PushMetaToDpu(const char * server_name, struct doca_comm_channel_ep
     return result;
   }
   for (auto &entry : *plasma_meta) {
-    printf("RunDmaExport1\n");  
+    // printf("RunDmaExport1\n");  
 
     // metainfo.object_id =  entry.first;
     // metainfo.allocation =  entry.second->GetAllocation();
@@ -162,10 +162,10 @@ doca_error_t PushMetaToDpu(const char * server_name, struct doca_comm_channel_ep
     for(int i=0; i<meta_info.export_desc_len; ++i) {
       meta_info.export_desc[i] = export_desc[i];
     }
-    printf("RunDmaExport2\n");
+    // printf("RunDmaExport2\n");
 
     meta_info.export_desc[meta_info.export_desc_len] = '\0';
-    printf("RunDmaExport3\n");
+    // printf("RunDmaExport3\n");
 
     while ((result = doca_comm_channel_ep_sendto(ep, &meta_info, amsg_len, DOCA_CC_MSG_FLAG_NONE, peer_addr)) ==
           DOCA_ERROR_AGAIN) {
@@ -180,7 +180,7 @@ doca_error_t PushMetaToDpu(const char * server_name, struct doca_comm_channel_ep
       std::cout<< "Message was sent: " << doca_get_error_string(result)<<std::endl;
     }
     object_id_set.insert(entry.first.Binary());
-    printf("RunDmaExport4\n");
+    // printf("RunDmaExport4\n");
 
   }
   // free(export_desc);
