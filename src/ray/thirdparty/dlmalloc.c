4ee4adaf0608d862c91de694b45ecc44dffa11cd
duihuhu <duihuhu@163.com>
2023-03-28 16:04:20 +0800

    debug rdma start address in plasma

--- src/ray/thirdparty/dlmalloc.c
+++ src/ray/thirdparty/dlmalloc.c
@@ -4554,6 +4554,8 @@ static void* tmalloc_small(mstate m, size_t nb) {
 #if !ONLY_MSPACES
 
 void* dlmalloc(size_t bytes) {
+  printf("in dlmalloc\n");
+
   /*
      Basic algorithm:
      If a small request (< 256 bytes minus per-chunk overhead):
