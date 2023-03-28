13260560801347e482a63bd4e7ab61dd5f19d0cc
duihuhu <duihuhu@163.com>
2023-03-28 14:47:54 +0800

    debug dadd create object when small than 100K

--- src/ray/object_manager/plasma/plasma_allocator.cc
+++ src/ray/object_manager/plasma/plasma_allocator.cc
@@ -83,7 +83,7 @@ PlasmaAllocator::PlasmaAllocator(const std::string &plasma_directory,
   // This will unmap the file, but the next one created will be as large
   // as this one (this is an implementation detail of dlmalloc).
 
-  RAY_LOG(WARNING) << " Free " << allocation.value();
+  RAY_LOG(WARNING) << " Free ";
 
 
   Free(std::move(allocation.value()));
