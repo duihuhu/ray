a538cd77155bcad1c1cdb2e526d8e3c2dfc912de
duihuhu <duihuhu@163.com>
2023-03-28 15:22:34 +0800

    debug rdma start address in plasma

--- src/ray/thirdparty/dlmalloc.c
+++ src/ray/thirdparty/dlmalloc.c
@@ -4053,6 +4053,7 @@ static void* sys_alloc(mstate m, size_t nb) {
   ensure_initialization();
   /* Directly map large chunks, but only if already initialized */
   if (use_mmap(m) && nb >= mparams.mmap_threshold && m->topsize != 0) {
+    printf("mmap_alloc\n");
     void* mem = mmap_alloc(m, nb);
     if (mem != 0)
       return mem;
