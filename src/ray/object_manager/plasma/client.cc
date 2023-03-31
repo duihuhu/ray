c242caa122bf88bcef94462e4e55c4baf1360c6f
duihuhu <duihuhu@163.com>
2023-03-31 11:12:57 +0800

    debug add get object meta from PopulateObjectStatus in core_worker.cc to ProcessMessage store.cc

--- src/ray/object_manager/plasma/client.cc
+++ src/ray/object_manager/plasma/client.cc
@@ -692,9 +692,9 @@ Status PlasmaClient::Impl::GetObjectMeta(const ObjectID &object_id) {
 
   RAY_LOG(DEBUG) << "ReadMetaReply GetObjectMeta " << object_id << " address " << address << " object_size " << object_size << " device_num " <<  device_num;
 
-  char *virt_address = (char*) address;
-  printf("virt_address %p", virt_address);
-
+  void *virt_address = (void*) address;
+  printf("virt_address %p\n", virt_address);
+  RAY_LOG(DEBUG) << "virt_address virt_address" << virt_address;
   // std::vector<ObjectBuffer> buffer;
   // RAY_RETURN_NOT_OK(PlasmaReceive(store_conn_, MessageType::PlasmaMetaReply, &buffer));
 
