1a209f9f445a8d61442f1af91b87f98ce58ef0f6
duihuhu <duihuhu@163.com>
2023-03-31 09:45:24 +0800

    debug add get object meta from PopulateObjectStatus in core_worker.cc to ProcessMessage store.cc

--- src/ray/object_manager/plasma/protocol.cc
+++ src/ray/object_manager/plasma/protocol.cc
@@ -682,7 +682,7 @@ Status ReadMetaRequest(uint8_t *data, size_t size, ObjectID *object_id) {
   return Status::OK();
 }
 
-Status SendMetaReply(const std::shared_ptr<Client> &client, unsigned long &address, int64_t &object_size, int &device_num) {
+Status SendMetaReply(const std::shared_ptr<Client> &client, unsigned long address, int64_t object_size, int device_num) {
   flatbuffers::FlatBufferBuilder fbb;
   auto message = fb::CreatePlasmaGetMetaReply(fbb, address, object_size, device_num);
   return PlasmaSend(client, MessageType::PlasmaGetMetaReply, &fbb, message);
