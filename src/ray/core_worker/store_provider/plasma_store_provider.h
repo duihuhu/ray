13912412c44a6939d0ab439ce13181d6a02ade93
duihuhu <duihuhu@163.com>
2023-03-31 11:27:01 +0800

    debug add get object meta from PopulateObjectStatus in core_worker.cc to ProcessMessage store.cc

--- src/ray/core_worker/store_provider/plasma_store_provider.h
+++ src/ray/core_worker/store_provider/plasma_store_provider.h
@@ -175,7 +175,7 @@ class CoreWorkerPlasmaStoreProvider {
 
   /// hucc get object meta from plasma.
   /// \param[in] object The object to create.
-  Status GetObjectMetaFromPlasma(const ObjectID &object_id);
+  Status GetObjectMetaFromPlasma(const ObjectID &object_id, unsigned long *address, int64_t *object_size, int *device_num);
      
   /// Lists objects in used (pinned) by the current client.
   ///
