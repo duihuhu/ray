64d40a01c26ba124cc2e0e252285acb0926dfeea
duihuhu <duihuhu@163.com>
2023-04-03 21:07:27 +0800

    debug add virt address in get object path

--- src/ray/raylet/node_manager.cc
+++ src/ray/raylet/node_manager.cc
@@ -1011,16 +1011,15 @@ void NodeManager::NodeAdded(const GcsNodeInfo &node_info) {
         }
       }));
 
+  std::string remote_node_manager_address = node_info.node_manager_address();
+  int64_t remote_register_time = node_info.register_time();
   if (self_register_time_ == 0) {
     RAY_LOG(DEBUG) << "self_register_time_ is not init";
     remote_node_register_time_[node_id] = std::make_pair(node_info.node_manager_address(), node_info.register_time());
     //todo need one hash to record which is already connect
-    // object_manager_rdma_.ConnectAndEx(entry.second.first);
-
+    object_manager_rdma_.ConnectAndEx(remote_node_manager_address);
   } else {
     RAY_LOG(DEBUG) << "self_register_time_ is already init " << node_info.node_manager_address();
-    std::string remote_node_manager_address = node_info.node_manager_address();
-    int64_t remote_register_time = node_info.register_time();
     if (self_register_time_ > remote_register_time) {
       object_manager_rdma_.ConnectAndEx(remote_node_manager_address);
       RAY_LOG(DEBUG) << "Accomplish ConnectAndEx with " << remote_node_manager_address;
