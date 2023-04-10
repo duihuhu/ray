f9e8cf788a323034304a41689f5304a02f865f3e
duihuhu <duihuhu@163.com>
2023-04-10 17:08:04 +0800

    debug post send and object_manager object in object_manager_rdma

--- src/ray/object_manager/object_manager_rdma.cc
+++ src/ray/object_manager/object_manager_rdma.cc
@@ -476,17 +476,14 @@ int ObjectManagerRdma::PostSend(struct pingpong_context *ctx, struct pingpong_de
 	sr.opcode = IBV_WR_RDMA_READ;
 	sr.send_flags = IBV_SEND_SIGNALED;
 	if (opcode != IBV_WR_SEND) {
-    RAY_LOG(DEBUG) << "opcode IBV_WR_RDMA_READ ";
 		sr.wr.rdma.remote_addr = remote_address;
 		sr.wr.rdma.rkey	= rem_dest->rkey;
 	}
 	rc = ibv_post_send(ctx->qp, &sr, &bad_wr);
-
+  RAY_LOG(DEBUG) << "ibv_post_send rc" << rc;
 	if (rc)
     RAY_LOG(ERROR) << "failed to post sr";
 	else {
-    RAY_LOG(DEBUG) << "opcode rc ";
-
 		switch (opcode) {
       case IBV_WR_SEND:
         // fprintf(stdout, "Send request was posted\n");
