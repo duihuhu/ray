39cd470c6426ea5ccc151d715c6f19b1c5b3807e
duihuhu <duihuhu@163.com>
2023-04-10 17:01:37 +0800

    debug post send and object_manager object in object_manager_rdma

--- src/ray/object_manager/object_manager_rdma.cc
+++ src/ray/object_manager/object_manager_rdma.cc
@@ -473,7 +473,7 @@ int ObjectManagerRdma::PostSend(struct pingpong_context *ctx, struct pingpong_de
 	sr.wr_id = 0;
 	sr.sg_list = &sge;
 	sr.num_sge = 1;
-	sr.opcode = IBV_WR_RDMA_READ;
+	sr.opcode = opcode;
 	sr.send_flags = IBV_SEND_SIGNALED;
 	if (opcode != IBV_WR_SEND) {
 		sr.wr.rdma.remote_addr = remote_address;
