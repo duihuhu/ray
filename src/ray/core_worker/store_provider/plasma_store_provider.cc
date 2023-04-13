c4d512eaf8d0a5f286acdea534460d6560ada56f
duihuhu <duihuhu@163.com>
2023-04-13 10:12:21 +0800

    debug insert object info into object_table_

--- src/ray/core_worker/store_provider/plasma_store_provider.cc
+++ src/ray/core_worker/store_provider/plasma_store_provider.cc
@@ -438,7 +438,7 @@ Status CoreWorkerPlasmaStoreProvider::Get(
 
     RAY_RETURN_NOT_OK(FetchAndGetFromPlasmaStore(remaining,
                                                  batch_ids,
-                                                 batch_timeout,
+                                                 0,
                                                  /*fetch_only=*/false,
                                                  ctx.CurrentTaskIsDirectCall(),
                                                  ctx.GetCurrentTaskID(),
