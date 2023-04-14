c80210d24ee0b32dde1af2c091de1d274440fb6e
duihuhu <duihuhu@163.com>
2023-04-14 18:45:39 +0800

    debug add batch_rem_ip_address in plasma_virt_info

--- src/ray/core_worker/store_provider/plasma_store_provider.h
+++ src/ray/core_worker/store_provider/plasma_store_provider.h
@@ -219,7 +219,7 @@ class CoreWorkerPlasmaStoreProvider {
       const std::vector<std::string> &batch_owner_ip_address,
       const std::vector<int> &batch_owner_port,
       const std::vector<ray::WorkerID> &batch_owner_worker_id,
-      const std::vector<ray::string> &batch_rem_ip_address);
+      const std::vector<std::string> &batch_rem_ip_address);
 
   /// Print a warning if we've attempted the fetch for too long and some
   /// objects are still unavailable.
