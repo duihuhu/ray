e88398a3acbbbcf7e599cf169b83004916eff3f5
duihuhu <duihuhu@163.com>
2023-04-14 15:36:19 +0800

    debug add checkinsertobjectinfo in object rdma

--- src/ray/object_manager/object_manager_rdma.cc
+++ src/ray/object_manager/object_manager_rdma.cc
@@ -442,7 +442,7 @@ void ObjectManagerRdma::FetchObjectFromRemotePlasma(const ray::WorkerID &worker_
     if(it!=remote_dest_.end())
       // continue;
       QueryQp(it->second.first.first);
-      if(object_manager_.CheckInsertObjectInfo(obj_info)):
+      if(object_manager_.CheckInsertObjectInfo(obj_info))
         continue;
 
       auto allocation = object_manager_.AllocateObjectSizeRdma(object_sizes[i]);
