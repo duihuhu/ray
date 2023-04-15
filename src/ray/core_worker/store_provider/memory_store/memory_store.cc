5630f70447bd0d21e464d66040809e204b4664f6
duihuhu <duihuhu@163.com>
2023-04-15 17:09:31 +0800

    debug add object get push request

--- src/ray/core_worker/store_provider/memory_store/memory_store.cc
+++ src/ray/core_worker/store_provider/memory_store/memory_store.cc
@@ -124,8 +124,7 @@ void GetRequest::Wait() {
 void GetRequest::Set(const ObjectID &object_id, std::shared_ptr<RayObject> object) {
   std::unique_lock<std::mutex> lock(mutex_);
   if (is_ready_) {
-    Ray_LOG(DEBUG) << "is_ready_  already" << is_ready_;
-
+    RAY_LOG(DEBUG) << "is_ready_  already " << is_ready_;
     return;  // We have already hit the number of objects to return limit.
   }
   object->SetAccessed();
@@ -134,7 +133,7 @@ void GetRequest::Set(const ObjectID &object_id, std::shared_ptr<RayObject> objec
   if (objects_.size() == num_objects_ ||
       (abort_if_any_object_is_exception_ && object->IsException() &&
        !object->IsInPlasmaError())) {
-    Ray_LOG(DEBUG) << "is_ready_ " << is_ready_;
+    RAY_LOG(DEBUG) << "is_ready_ " << is_ready_;
     is_ready_ = true;
     cv_.notify_all();
   }
