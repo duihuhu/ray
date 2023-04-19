e56bd672c0db71d16d117e241921debb87c7dc35
Fangzhe <gaofz@FangzhedeMacBook-Pro.local>
2023-04-19 20:49:41 +0800

    debug change rdma rx_depth

--- src/ray/object_manager/object_manager_rdma.cc
+++ src/ray/object_manager/object_manager_rdma.cc
@@ -503,7 +503,11 @@ int ObjectManagerRdma::PostSend(struct pingpong_context *ctx, struct pingpong_de
     sge.addr = buf;
 	RAY_LOG(ERROR) << "PostSend median2";
 	sge.length = msg_size;
+	RAY_LOG(ERROR) << "PostSend median3";
+
 	sge.lkey = ctx->mr->lkey;
+	RAY_LOG(ERROR) << "PostSend median4";
+
 	memset(&sr, 0, sizeof(sr));
 	sr.next = NULL;
 	sr.wr_id = 0;
@@ -511,7 +515,7 @@ int ObjectManagerRdma::PostSend(struct pingpong_context *ctx, struct pingpong_de
 	sr.num_sge = 1;
 	sr.opcode = IBV_WR_RDMA_READ;
 	sr.send_flags = IBV_SEND_SIGNALED;
-	RAY_LOG(ERROR) << "PostSend median3";
+	RAY_LOG(ERROR) << "PostSend median5";
 
 	if (opcode != IBV_WR_SEND) {
 		sr.wr.rdma.remote_addr = remote_address;
