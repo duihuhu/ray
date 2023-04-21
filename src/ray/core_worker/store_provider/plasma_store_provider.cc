ce2ee84eab44b764b5703e148c212470fd460d3d
Fangzhe <gaofz@FangzhedeMacBook-Pro.local>
2023-04-21 14:35:38 +0800

    debug change rdma rx_depth

--- src/ray/core_worker/store_provider/plasma_store_provider.cc
+++ src/ray/core_worker/store_provider/plasma_store_provider.cc
@@ -473,9 +473,9 @@ Status CoreWorkerPlasmaStoreProvider::Get(
                                                  batch_rem_ip_address));
     should_break = timed_out || *got_exception;
 
-    if ((previous_size - remaining.size()) < batch_ids.size()) {
-      WarnIfFetchHanging(fetch_start_time_ms, remaining);
-    }
+    // if ((previous_size - remaining.size()) < batch_ids.size()) {
+    //   WarnIfFetchHanging(fetch_start_time_ms, remaining);
+    // }
     //hucc time for get obj from remote plasma
     auto te_get_obj_remote_plasma = current_sys_time_us();
     RAY_LOG(WARNING) << "hucc time for get obj from local plasma total time in while: " << te_get_obj_remote_plasma - ts_get_obj_remote_plasma << " empty: " << remaining.empty() << "\n";
