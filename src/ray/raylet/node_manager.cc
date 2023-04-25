751a736490901a2874d1e05ba05a341341543245
duihuhu <duihuhu@163.com>
2023-04-25 16:09:46 +0800

    debug edit dependencymanager insert local object info

--- src/ray/raylet/node_manager.cc
+++ src/ray/raylet/node_manager.cc
@@ -341,7 +341,7 @@ NodeManager::NodeManager(instrumented_io_context &io_service,
       node_manager_server_("NodeManager",
                            config.node_manager_port,
                            config.node_manager_address == "127.0.0.1",
-                           config_.rpc_service_threads_number),
+                           object_manager_config.rpc_service_threads_number),
       node_manager_service_(io_service, *this),
       agent_manager_service_handler_(
           new DefaultAgentManagerServiceHandler(agent_manager_)),
