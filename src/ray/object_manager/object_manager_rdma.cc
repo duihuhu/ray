4903939d67bbd8af596d10500f7f5c4fae837914
duihuhu <duihuhu@163.com>
2023-04-28 16:44:57 +0800

    debug change object_manager_rdma to mul threads with consumer and producer, edit allocate object size

--- src/ray/object_manager/object_manager_rdma.cc
+++ src/ray/object_manager/object_manager_rdma.cc
@@ -53,9 +53,10 @@ void ObjectManagerRdma::FetchObjectFromRemotePlasmaThreads(ObjectRdmaInfo &objec
 
 		auto pair = object_manager_.CreateObjectRdma(object_rdma_info.object_info);
 		RAY_LOG(DEBUG) << " Allocate pair ";
-		// if (pair.first == nullptr) {
-		// 	continue;
-		// }
+		auto entry = pair.first;
+		if (entry == nullptr) {
+			continue;
+		}
 		auto allocation = pair.first->GetAllocation();
 
 		RAY_LOG(DEBUG) << " Allocate space allocation->address " << allocation.address << " object_id " << object_rdma_info.object_info.object_id;
