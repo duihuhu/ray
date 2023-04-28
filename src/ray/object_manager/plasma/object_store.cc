27db84f6a282a2a6ad7dc99147f1975eeeed5ce2
duihuhu <duihuhu@163.com>
2023-04-28 16:18:25 +0800

    debug change object_manager_rdma to mul threads with consumer and producer, edit allocate object size

--- src/ray/object_manager/plasma/object_store.cc
+++ src/ray/object_manager/plasma/object_store.cc
@@ -96,6 +96,7 @@ bool ObjectStore::DeleteObject(const ObjectID &object_id) {
   }
   allocator_.Free(std::move(entry->allocation));
   object_table_.erase(object_id);
+  object_table_rdma_.earse(object_id);
   return true;
 }
 
