cf689b52e8ebbd5e9658eefc6a9f761957b221e4
duihuhu <duihuhu@163.com>
2023-05-06 19:23:57 +0800

    debug change object_manager_rdma to mul threads with consumer and producer, edit allocate object size

--- src/ray/object_manager/plasma/store.cc
+++ src/ray/object_manager/plasma/store.cc
@@ -305,11 +305,11 @@ std::pair<const LocalObject *, flatbuf::PlasmaError> PlasmaStore::CreateObjectRd
   //                 << " bytes";
   // ProcessCreateRequests();
   // ReplyToCreateClient(client, object_id, req_id);
-
+  {
   absl::MutexLock lock(&mutex_);
-  RAY_LOG(DEBUG) << "before CreateObjectRdma address ";
-
-  auto pair = object_lifecycle_mgr_.CreateObjectRdma(object_info, source, fallback_allocator);
+    RAY_LOG(DEBUG) << "before CreateObjectRdma address ";
+    auto pair = object_lifecycle_mgr_.CreateObjectRdma(object_info, source, fallback_allocator);
+  }
   auto entry = pair.first;
   if (entry == nullptr) {
     return pair;
