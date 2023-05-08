6467bf1f20a9a7ea126c51df188bf86d2ba50d82
duihuhu <duihuhu@163.com>
2023-05-08 11:26:06 +0800

    debug change object_manager_rdma to mul threads with consumer and producer, edit allocate object size

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -1208,7 +1208,7 @@ Status CoreWorker::Get(const std::vector<ObjectID> &ids,
     RAY_CHECK(!missing_result);
   }
   auto te_get_obj_cw = current_sys_time_us();
-  RAY_LOG(INFO) << "hucc time for add get object in coreworker total time: " << te_get_obj_cw - ts_get_obj_cw <<"\n";
+  RAY_LOG(INFO) << "hucc time for add get object in coreworker total time: " << te_get_obj_cw - ts_get_obj_cw << " "  << ids[0];
 
   return Status::OK();
 }
