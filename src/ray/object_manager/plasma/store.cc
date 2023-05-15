0e50a658e10f810bf868d38bb21cfe2fb8079710
duihuhu <duihuhu@163.com>
2023-05-15 15:59:31 +0800

    debug rdma seal object

--- src/ray/object_manager/plasma/store.cc
+++ src/ray/object_manager/plasma/store.cc
@@ -335,7 +335,8 @@ void PlasmaStore::InsertObjectInfoThread(const Allocation& allocation , const ra
   absl::MutexLock lock(&mutex_);
   object_lifecycle_mgr_.InsertObjectInfoThread(allocation, object_info, pair);
   RAY_CHECK(object_lifecycle_mgr_.AddReferenceRdma(object_info.object_id, pair.first));
-  SealObjects(object_info.object_id);
+  auto entry = object_lifecycle_mgr_.SealObject(object_info.object_id);
+  add_object_callback_(entry->GetObjectInfo());
 }
 
 
