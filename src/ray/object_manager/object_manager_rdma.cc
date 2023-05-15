03650686f26dea99187473fe8528b55a8a8b673f
duihuhu <duihuhu@163.com>
2023-05-15 16:06:57 +0800

    debug rdma seal object

--- src/ray/object_manager/object_manager_rdma.cc
+++ src/ray/object_manager/object_manager_rdma.cc
@@ -114,7 +114,7 @@ int ObjectManagerRdma::PollCompletionThreads(struct pingpong_context *ctx, const
 
       // object_manager_.InsertObjectInfo(allocation, object_info);
 			object_manager_.InsertObjectInfoThread(allocation, object_info, pair);
-			dependency_manager_->InsertObjectInfo(object_info);
+			// dependency_manager_->InsertObjectInfo(object_info);
 			auto ts_fetch_object_end = current_sys_time_us();
 			// RAY_LOG(DEBUG) << "plasma client fetch object id start: " << object_info.object_id << " " << ts_fetch_object_end;
 			RAY_LOG(DEBUG) << "plasma client fetch object id start: " << object_info.object_id << " " << ts_fetch_object_end << " " << ts_fetch_rdma << " " << te_fetch_object_post_send << " " << start_time;
