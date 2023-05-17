fbc3c95e5a97915389b013a9a1c9b173a1c84b96
duihuhu <duihuhu@163.com>
2023-05-17 15:07:55 +0800

    debug py buffer and data

--- src/ray/core_worker/store_provider/plasma_store_provider.cc
+++ src/ray/core_worker/store_provider/plasma_store_provider.cc
@@ -291,18 +291,19 @@ Status CoreWorkerPlasmaStoreProvider::FetchAndGetFromPlasmaStoreRDMA(
         // outfile<<"\n";
         // outfile<<plasma_results[i].data->Data();
         // outfile.close();
-      }
-      if (plasma_results[i].metadata && plasma_results[i].metadata->Size()) {
-        metadata = plasma_results[i].metadata;
-        			// object info
+                			// object info
         RAY_LOG(ERROR) << object_id <<  " " << object_id.Hash()<< " " << plasma_results[i].data.get()->Size();
         std::ofstream outfile1;
         outfile1.open("hutmp_" + std::to_string(object_id.Hash()) + "data.txt");
 
         for(int i=0; i<plasma_results[i].data.get()->Size(); ++i){
-          outfile1<<(plasma_results[i].data.get()->Data())[i];
+          outfile1<<(data.get()->Data())[i];
         }
         outfile1.close();
+      }
+      if (plasma_results[i].metadata && plasma_results[i].metadata->Size()) {
+        metadata = plasma_results[i].metadata;
+
         // RAY_LOG(ERROR) << object_id <<  " " << object_id.Hash() << " " <<  plasma_results[i].metadata->Data();
         // std::ofstream outfile1;
         // outfile1.open("hutmp_" + std::to_string(object_id.Hash()) + "metadata.txt");
