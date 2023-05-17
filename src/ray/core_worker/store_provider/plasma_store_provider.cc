afdf61f203b06e41fe9c94854ecc9d0acadd6840
duihuhu <duihuhu@163.com>
2023-05-17 11:07:42 +0800

    debug py buffer and data

--- src/ray/core_worker/store_provider/plasma_store_provider.cc
+++ src/ray/core_worker/store_provider/plasma_store_provider.cc
@@ -287,13 +287,12 @@ Status CoreWorkerPlasmaStoreProvider::FetchAndGetFromPlasmaStoreRDMA(
       }
       if (plasma_results[i].metadata && plasma_results[i].metadata->Size()) {
         metadata = plasma_results[i].metadata;
-
         			// object info
         RAY_LOG(ERROR) << object_id <<  " " << object_id.Hash();
         std::ofstream outfile1;
         outfile1.open("hutmp_" + std::to_string(object_id.Hash()) + ".txt");
         for(int i=0; i< plasma_results[i].metadata->Size(); ++i){
-          outfile1<<*(metadata->get()->Data()+i);
+          outfile1<<*(metadata->Data()+i);
         }
         // outfile1<<plasma_results[i].data->Size();
         // outfile1<<"\n";
