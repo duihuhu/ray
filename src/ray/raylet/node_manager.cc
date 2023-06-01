db99a534a8a1538244ccada14a1c2c6be996c04a
duihuhu <duihuhu@163.com>
2023-06-01 10:45:22 +0800

    debug breakdown get object

--- src/ray/raylet/node_manager.cc
+++ src/ray/raylet/node_manager.cc
@@ -1762,7 +1762,7 @@ void NodeManager::ProcessFetchOrReconstructRDMAMessage(
   //   RAY_LOG(DEBUG) << "hucc breakdown get object nodemanager: " << ts_breakdown_get_object_node_manager << " object_id: " << obj_id << "\n";
   // }
   //end hucc 
-  RAY_LOG(ERROR) << " raylet client send 1 " << ts_fetch_or_restruct_message << " " << object_rdma_info[0].object_info.object_id;
+  // RAY_LOG(ERROR) << " raylet client send 1 " << ts_fetch_or_restruct_message << " " << object_rdma_info[0].object_info.object_id;
 
   if (message->fetch_only()) {
     std::shared_ptr<WorkerInterface> worker = worker_pool_.GetRegisteredWorker(client);
