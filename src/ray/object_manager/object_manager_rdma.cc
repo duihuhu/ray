f89e958bc755ce8361c9688e29e2ac80ba7e8269
duihuhu <duihuhu@163.com>
2023-06-15 21:29:59 +0800

    debug add rdma to rdma server(continuing)

--- src/ray/object_manager/object_manager_rdma.cc
+++ src/ray/object_manager/object_manager_rdma.cc
@@ -208,17 +208,17 @@ void ObjectManagerRdma::HandleGetObject(const ray::rpc::GetObjectRequest &reques
 	std::vector<ObjectRdmaInfo> object_rdma_info;
 	for (int i = 0;i <request.object_ids().size()) {
 		ObjectRdmaInfo obj_rdma_info;
-		obj_rdma_info.object_virt_address = request.virt_address.Get(i);
-    obj_rdma_info.object_sizes = request.object_size.Get(i) + request.meta_size.Get(i);
+		obj_rdma_info.object_virt_address = request.virt_address().Get(i);
+    obj_rdma_info.object_sizes = request.object_size().Get(i) + request.meta_size().Get(i);
     obj_rdma_info.object_address = addr->ip_address()->str();
-    obj_rdma_info.object_info.object_id = ObjectID::FromBinary(request.object_ids.Get(i)->str());
-    obj_rdma_info.object_info.data_size = request.object_size.Get(i);
-    obj_rdma_info.object_info.metadata_size = request.meta_size.Get(i);
-    obj_rdma_info.object_info.owner_raylet_id = NodeID::FromBinary(request.owner_raylet_id.Get(i)->str());
-    obj_rdma_info.object_info.owner_ip_address = request.owner_ip_address.Get(i);
-    obj_rdma_info.object_info.owner_port = request.owner_port[i];
-    obj_rdma_info.object_info.owner_worker_id = WorkerID::FromBinary(request.owner_worker_id.Get(i)->str());
-    obj_rdma_info.rem_ip_address = request.rem_ip_address[i];
+    obj_rdma_info.object_info.object_id = ray::ObjectID::FromBinary(request.object_ids().Get(i)->str());
+    obj_rdma_info.object_info.data_size = request.object_size().Get(i);
+    obj_rdma_info.object_info.metadata_size = request.meta_size().Get(i);
+    obj_rdma_info.object_info.owner_raylet_id = ray::NodeID::FromBinary(request.owner_raylet_id().Get(i)->str());
+    obj_rdma_info.object_info.owner_ip_address = request.owner_ip_address().Get(i);
+    obj_rdma_info.object_info.owner_port = request.owner_port().Get(i);
+    obj_rdma_info.object_info.owner_worker_id = ray::WorkerID::FromBinary(request.owner_worker_id.Get(i)->str());
+    obj_rdma_info.rem_ip_address = request.rem_ip_address().Get(i);
 
     object_rdma_info.emplace_back(obj_rdma_info);
 	}
