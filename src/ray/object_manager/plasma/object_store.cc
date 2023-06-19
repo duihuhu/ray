7df571f197066cdda586c1b419d4e5f7dd3c7f44
duihuhu <duihuhu@163.com>
2023-06-19 21:59:48 +0800

    debug add rdma to rdma server(continuing)

--- src/ray/object_manager/plasma/object_store.cc
+++ src/ray/object_manager/plasma/object_store.cc
@@ -27,7 +27,8 @@ const LocalObject *ObjectStore::CreateObject(const ray::ObjectInfo &object_info,
                                              bool fallback_allocate, bool rdma) {
   // absl::MutexLock lock(&mutex_);
   auto ts_create_object = current_sys_time_us()
-  RAY_LOG(DEBUG) << "attempting to create object " << ts_create_object << " " << object_info.object_id ;
+  RAY_LOG(ERROR) << "attempting to create object " << ts_create_object << " " << object_info.object_id;
+
   RAY_LOG(DEBUG) << "attempting to create object " << object_info.object_id << " size "
                  << object_info.data_size;
   // RAY_LOG(WARNING) << "attempting to create object " << object_info.object_id << " size "
