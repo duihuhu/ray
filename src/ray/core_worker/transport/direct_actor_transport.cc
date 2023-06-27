e02e3f7362c26935e184c3777f560f8c852c0b87
duihuhu <duihuhu@163.com>
2023-06-27 11:43:04 +0800

    debug uncomment delete

--- src/ray/core_worker/transport/direct_actor_transport.cc
+++ src/ray/core_worker/transport/direct_actor_transport.cc
@@ -194,8 +194,8 @@ void CoreWorkerDirectTaskReceiver::HandleTask(
       }
       for (size_t i = 0; i < return_objects.size(); i++) {
         const auto &return_object = return_objects[i];
-        auto ts_return_object = current_sys_time_us();
-        RAY_LOG(ERROR) << "before return object " << " " << te_exec_task- ts_exec_task << " " <<  ts_return_object << " " << return_object.first;
+        // auto ts_return_object = current_sys_time_us();
+        // RAY_LOG(ERROR) << "before return object " << " " << te_exec_task- ts_exec_task << " " <<  ts_return_object << " " << return_object.first;
         auto return_object_proto = reply->add_return_objects();
         SerializeReturnObject(
             return_object.first, return_object.second, return_object_proto, plasma_store_provider_);
