bb2675546f06d5b54cb2cafd05104c85c56379d6
duihuhu <duihuhu@163.com>
2023-07-07 11:22:51 +0800

    debug add mult object in one get

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -1225,9 +1225,9 @@ Status CoreWorker::Get(const std::vector<ObjectID> &ids,
   if (timeout_ms < 0 && !will_throw_exception) {
     RAY_CHECK(!missing_result);
   }
-  auto te_get_obj_cw = current_sys_time_us();
-  RAY_LOG(ERROR) << "hucc time for add get object in coreworker total time: " << te_get_obj_cw - ts_get_obj_cw << " get from plasma: " << \
-    te_get_obj_cw - ts_get_obj_plasma << " wait in mem: " << ts_get_obj_plasma - ts_get_obj_cw << " " << te_get_obj_cw << " " << ts_get_obj_cw << " " << ts_get_obj_plasma;
+  // auto te_get_obj_cw = current_sys_time_us();
+  // RAY_LOG(ERROR) << "hucc time for add get object in coreworker total time: " << te_get_obj_cw - ts_get_obj_cw << " get from plasma: " << \
+  //   te_get_obj_cw - ts_get_obj_plasma << " wait in mem: " << ts_get_obj_plasma - ts_get_obj_cw << " " << te_get_obj_cw << " " << ts_get_obj_cw << " " << ts_get_obj_plasma;
     
   // RAY_LOG(INFO) << "hucc time for add get object in coreworker total time: " << te_get_obj_cw - ts_get_obj_cw << " "  << ids[0]; 
   // if(ids.size()>0) {
