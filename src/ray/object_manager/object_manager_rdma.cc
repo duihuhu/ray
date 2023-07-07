3928fc8afc44050e1dd7a6c33e7dcbbe4763e830
duihuhu <duihuhu@163.com>
2023-07-07 11:49:05 +0800

    debug add mult object in one get

--- src/ray/object_manager/object_manager_rdma.cc
+++ src/ray/object_manager/object_manager_rdma.cc
@@ -145,7 +145,8 @@ int ObjectManagerRdma::PollCompletionThreads(struct pingpong_context *ctx, const
 			// RAY_LOG(ERROR) << "after " << object_info.object_id <<  " " << *(data+object_info.data_size);
 
 			while(data[object_info.data_size]!='P' || data[object_info.data_size]!='R' || data[object_info.data_size]!='X' || data[object_info.data_size]!='p' || data[object_info.data_size]!='r' || data[object_info.data_size]!='x') {
-				RAY_LOG(ERROR) << "object_id " << object_info.object_id << " data is:" << data[object_info.data_size];
+				bool flag = data[object_info.data_size]!='P';
+				RAY_LOG(ERROR) << "object_id " << object_info.object_id << " data is:" << data[object_info.data_size] << " " << flag;
 				std::this_thread::sleep_for(std::chrono::microseconds(10));
 			}
 
