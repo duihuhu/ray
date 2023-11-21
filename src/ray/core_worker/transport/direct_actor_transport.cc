a750c7950717aa4e8b3014625bb8bfaf0e59e0e2
duihuhu <duihuhu@163.com>
2023-11-21 13:44:24 +0800

    debug add plasma_store_provider in serial

--- src/ray/core_worker/transport/direct_actor_transport.cc
+++ src/ray/core_worker/transport/direct_actor_transport.cc
@@ -179,13 +179,13 @@ void CoreWorkerDirectTaskReceiver::HandleTask(
       for (const auto &dynamic_return : dynamic_return_objects) {
         auto return_object_proto = reply->add_dynamic_return_objects();
         SerializeReturnObject(
-            dynamic_return.first, dynamic_return.second, return_object_proto);
+            dynamic_return.first, dynamic_return.second, return_object_proto, plasma_store_provider_);
       }
       for (size_t i = 0; i < return_objects.size(); i++) {
         const auto &return_object = return_objects[i];
         auto return_object_proto = reply->add_return_objects();
         SerializeReturnObject(
-            return_object.first, return_object.second, return_object_proto);
+            return_object.first, return_object.second, return_object_proto, plasma_store_provider_);
       }
 
       if (task_spec.IsActorCreationTask()) {
