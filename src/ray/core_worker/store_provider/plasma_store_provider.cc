8a7ce594c654d1183ecd3633ad8ce764f183a74d
duihuhu <duihuhu@163.com>
2024-02-28 22:35:06 +0800

    debug object type

--- src/ray/core_worker/store_provider/plasma_store_provider.cc
+++ src/ray/core_worker/store_provider/plasma_store_provider.cc
@@ -755,7 +755,7 @@ Status CoreWorkerPlasmaStoreProvider::GetRDMA(
         continue;
       }
       else{
-        RAY_LOG(ERROR)<<"batch remain info " << it->second.first.first << " " << it->second.second.owner_ip_address << " " << id << " is actorid " << ray::IsActorID(id) ;
+        RAY_LOG(ERROR)<<"batch remain info " << it->second.first.first << " " << it->second.second.owner_ip_address << " " << id << " is actorid " << ray::Object::IsActorID(id) ;
       }
       batch_ids.push_back(id);
       batch_virt_address.push_back(it->second.first.first);
